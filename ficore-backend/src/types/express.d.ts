cd3b36670210e833f6cc2b51677f151d0fe55e2c
Hassanksg <91823350+Hassanksg@users.noreply.github.com>
2025-08-26 11:58:18 +0100

    Update express.d.ts

--- ficore-backend/src/types/express.d.ts
+++ ficore-backend/src/types/express.d.ts
@@ -8,14 +8,3 @@ declare module 'express' {
 
 }
 
-// src/types/express/index.d.ts
-
-import { UserDocument } from '../../models/user';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: UserDocument;
-    }
-  }
-}
